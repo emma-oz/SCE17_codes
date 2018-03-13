0f52d420a2eaa171187c9422818cf17c3fbe8f1f
ecreeves <ecreeves@ucsd.edu>
2018-03-13 13:12:05 -0700

    update GPS ship data

--- select_ships.m
+++ select_ships.m
@@ -1,9 +1,10 @@
 % selects ship paths for machine learning
+% this basically just interpolates the gps data to consistent time vector
 % Emma Ozanich, 12/14/2017
 
  % % LOAD % %
 load('SCE17_AllShips');
-PLOT = true;
+PLOT = false;
 JD = 84; % not used currently
 ploton = 1; % plot ship paths
 
@@ -16,11 +17,12 @@ load('Flatlon.mat');
  g = fieldnames(GPS);
  pictick = 1;
  for ii = 1:length(g)
+     tic;
    %  if sum(GPS.(g{ii}).lat < VLA2(2)) == length(GPS.(g{ii}).lat)
          Ships.(g{ii}) = GPS.(g{ii});
          
      % % Compute ranges and azimuths for south ships % %    
-         [Ships.(g{ii}).range, Ships.(g{ii}).azimuth] = distance(VLA2(2), VLA2(1), Ships.(g{ii}).lat, -Ships.(g{ii}).lon,...
+         [Ships.(g{ii}).range, Ships.(g{ii}).azimuth] = distance(VLA2(2), -VLA2(1), Ships.(g{ii}).lat, Ships.(g{ii}).lon,...
             [6378.273 0.0034],'degrees');
          Ships.(g{ii}).dtime = datenum(Ships.(g{ii}).dnum(1):seconds(1):Ships.(g{ii}).dnum(end));
          
@@ -32,18 +34,21 @@ load('Flatlon.mat');
          % interpolate
          % remove non-unique points
          dunique = unique(Ships.(g{ii}).dnum);
+         Ships.(g{ii}).R = Ships.(g{ii}).range;
+         Ships.(g{ii}).A = Ships.(g{ii}).azimuth;
          if length(dunique)~=length(Ships.(g{ii}).dnum)
             for jj = 1:length(dunique)
                 rng(jj) = mean(Ships.(g{ii}).range(Ships.(g{ii}).dnum == dunique(jj)));
                 azz(jj) = mean(Ships.(g{ii}).azimuth(Ships.(g{ii}).dnum == dunique(jj)));
             end
-            Ships.(g{ii}).range = rng;
-            Ships.(g{ii}).azimuth = azz;
+            Ships.(g{ii}).R = rng;
+            Ships.(g{ii}).A = azz;
+            clear rng azz
          end
   
          
-         Ships.(g{ii}).range = interp1(dunique, Ships.(g{ii}).range, Ships.(g{ii}).dtime,'linear');
-         Ships.(g{ii}).azimuth = interp1(dunique, Ships.(g{ii}).azimuth, Ships.(g{ii}).dtime,'linear');
+         Ships.(g{ii}).range = interp1(dunique, Ships.(g{ii}).R, Ships.(g{ii}).dtime,'linear');
+         Ships.(g{ii}).azimuth = interp1(dunique, Ships.(g{ii}).A, Ships.(g{ii}).dtime,'linear');
          
          if PLOT
             hold on;
@@ -58,7 +63,7 @@ load('Flatlon.mat');
 
             pictick = pictick+1;
          end
-         
+         toc
     % else
      %    disp('The ship traverses north of VLA2.');
    %  end  
