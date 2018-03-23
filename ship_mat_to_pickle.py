fdf324c6ab5c045243e03147985f0ea87234c880
ecreeves <ecreeves@ucsd.edu>
2018-03-23 14:13:35 -0700

    Files for full workflow
    Goes from raw GPS & acoustic data to post-processed inputs for FNN

---
+++ ship_mat_to_pickle.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 19 10:11:49 2018
+
+@author: emmacreeves
+"""
+import scipy.io as sio
+import numpy as np
+import pickle
+import glob
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import random
+
+savepath = 'data/VLA2/'
+
+inputs = sorted(glob.glob('input_*.mat'))
+ranges = sorted(glob.glob('range_*.mat'))
+
+# add the previous data sets
+#ranges = ranges + sorted(glob.glob('label_V*'))
+
+#del inputs[0]
+#del ranges[0]
+#del inputs[1]
+#del ranges[1]
+
+
+delind = [] # list of potential indices to remove
+useall = True
+total_data = []
+total_range = []
+
+
+train_range = [i for i in range(0,len(inputs)-1)]
+test_range = [i for i in range(len(inputs)-1, len(inputs))]
+if useall:
+    file_range = test_range
+else:
+    file_range = train_range
+
+for ii in file_range:
+    
+    if inputs[ii][-3:] == 'txt':
+         D = np.loadtxt(inputs[ii])
+         data = D
+         rng = np.loadtxt(ranges[ii])
+         rng = rng[0:int(len(rng)/2)]
+    elif inputs[ii][-3:] == 'mat':
+         D = sio.loadmat(inputs[ii])
+         label = sio.loadmat(ranges[ii])
+         data = D['out']
+         rng = label['range'][0,:]
+    else:
+         print('Error loading file type!')
+
+    print(inputs[ii],data.shape)
+    if data.shape[0]>len(rng):
+        data = data[0:len(rng),:]
+        print('Im not using data of mismatched length at this time: ' + inputs[ii])
+        delind.append(int(ii))
+        continue;
+    elif len(rng)>data.shape[0]:
+        rng = rng[0:data.shape[0]]
+        print('Im not using data of mismatched length at this time:' + inputs[ii])
+        delind.append(int(ii))
+        continue;
+    total_data.append(data)
+    total_range.append(rng)
+        
+data = total_data[0]
+for i in range(1, len(total_data)):
+    data=np.concatenate([data, total_data[i]])
+
+
+rng = total_range[0]
+for i in range(1, len(total_range)):
+    rng=np.concatenate([rng, total_range[i]])
+
+        
+ 
+
+cutoff = int(np.floor(len(rng)*0.3))
+randshuff = np.arange(0,len(rng))
+random.shuffle(randshuff)
+    
+    
+if useall: # turn the whole track into a test set
+    x_val = data
+    y_val = rng
+    print(data.shape, x_val.shape)
+else:
+    x_val = data[sorted(randshuff[0:cutoff]),:]
+    x_train = data[sorted(randshuff[cutoff:]),:]
+    y_val = rng[sorted(randshuff[0:cutoff])]
+    y_train = rng[sorted(randshuff[cutoff:])]
+    print(x_train.shape, x_val.shape)
+
+    
+if 'x_train' in locals():
+    pca = PCA(n_components=50)
+    pca.fit(x_train)
+    pickle.dump(pca,open(savepath + 'pca.p','wb'))
+    print("pca object saved!")
+    x_train = pca.transform(x_train) 
+    pickle.dump(x_train, open(savepath + 'x_train' + '.p','wb'))
+    pickle.dump(y_train, open(savepath + 'y_train' +  '.p','wb'))
+    test_file = "_val"
+else:
+    pca = pickle.load(open(savepath + 'pca.p', 'rb'))
+    print("pca object loaded!")
+    test_file = "_test"
+    
+x_val = pca.transform(x_val)
+pickle.dump(x_val, open(savepath + 'x'+test_file + '.p', 'wb'))
+pickle.dump(y_val, open(savepath + 'y'+test_file + '.p', 'wb'))
+
+
+inputs = [ item for i,item in enumerate(inputs) if i not in delind ]
+ranges = [ item for i,item in enumerate(ranges) if i not in delind ]   
--- workflow.m
+++ workflow.m
@@ -1,170 +1,249 @@
-%{/
+
+% reset
 clear;
 close all;
+warning off;
 
-pathtosioread = ''; %where's the sioread function?
+SELECT_SHIPS = True; % set True if you want to select new ship paths, False otherwise
+
+% % define paths % %
+pathtosioread = '';
 pathtodata = '../../project/SCE17/acoustic/SCE17_VLA2_sio/';
 load('InterpShips_SCE17.mat');
 addpath(genpath(pathtosioread));
 savepath = '';
 
+% % % data selection % % %
 ships = fieldnames(Ships);
+if SELECT_SHIPS
+    index = [];
+    xlims = [];
+    for ii = 1:length(ships)
 
-% % % select which ships tracks to load (using GPS) % % %
-index = [];
-xlims = [];
-for ii = 1:length(ships)
-   if ~sum(Ships.(ships{ii}).dtime >= datenum(2017,1,82,19,0,0)) % limitations of acoustics data
-      continue; 
-   else
-       posind = find(Ships.(ships{ii}).dtime >= datenum(2017,1,82,19,0,0));
-       Ships.(ships{ii}).range = Ships.(ships{ii}).range(posind);
-       Ships.(ships{ii}).azimuth = Ships.(ships{ii}).azimuth(posind);
-       Ships.(ships{ii}).dtime = Ships.(ships{ii}).dtime(posind);
-       postind = find(Ships.(ships{ii}).dnum >= datenum(2017,1,82,19,0,0));
-       Ships.(ships{ii}).lat = Ships.(ships{ii}).lat(postind);
-       Ships.(ships{ii}).lon = Ships.(ships{ii}).lon(postind);
-       Ships.(ships{ii}).R = Ships.(ships{ii}).R(postind);
-       Ships.(ships{ii}).A = Ships.(ships{ii}).A(postind);
-       Ships.(ships{ii}).dnum = Ships.(ships{ii}).dnum(postind);
-   end
-       
-   plot(Ships.(ships{ii}).dtime, Ships.(ships{ii}).range);
-   hold on;
-   scatter(Ships.(ships{ii}).dnum, Ships.(ships{ii}).R,'filled');
-   ylabel('Range (km)');
-   xlabel('Date, Time');
-   datetick('keeplimits');
-   title(['Ship name: ' ships{ii}]);
-   legend('Interpolated range','Raw range');
-   s = input('Do you want to use this ship track?  (Y/N)','s');
-   if strcmp(s,'Y')
-      index = [index; ii]; 
-      s = input('Do you want to use the whole time? (Y/N)','s');
-      if strcmp(s,'N')
-          disp('Select two points on the plot: starting point and ending point.');
-          dg = ginput(2);
-          xlim([dg(1,1) dg(2,1)]);
-          s = input('Does that look better? (Y/N)','s');
-          if strcmp(s,'N')
-              disp('Ok, try again...');
-              dg = ginput(2);
-              disp('Heres what you get:');
-              xlim([dg(1,1) dg(2,1)]);
-              pause(2);
-              disp('Moving on to next ship.');
-          end
-          [~,I1] = min(abs(dg(1,1) - Ships.(ships{ii}).dtime));
-          [~,I2] = min(abs(dg(2,1) - Ships.(ships{ii}).dtime));
-          xlims = [xlims; I1,I2];
-      else
-         xlims = [xlims; 1, length(Ships.(ships{ii}).dtime)]; 
-      end
-   elseif strcmp(s,'N')
-       s = input('Are you done? (Y/N)','s');
-       if strcmp(s,'Y')
-           disp('Thanks, closing program...');
-           break;
+        % don't run if outside hydrophone recording period
+       if ~sum(Ships.(ships{ii}).dtime >= datenum(2017,1,82,19,0,0)) || ...
+        ~sum(Ships.(ships{ii}).dtime <=datenum(2017,1,92,19,0,0)) 
+          continue; 
+
+       else
+           % delimit raw data
+           rawbin = (Ships.(ships{ii}).dnum >= datenum(2017,1,82,19,0,0)) & ...
+               (Ships.(ships{ii}).dnum <=datenum(2017,1,92,19,0,0));     
+           Ships.(ships{ii}).lat = Ships.(ships{ii}).lat(rawbin);
+           Ships.(ships{ii}).lon = Ships.(ships{ii}).lon(rawbin);
+
+           % delimit interpolated data
+           interpbin = ((Ships.(ships{ii}).dtime >= datenum(2017,1,82,19,0,0)) & ...
+                (Ships.(ships{ii}).dtime <=datenum(2017,1,92,19,0,0)));
+           Ships.(ships{ii}).range = Ships.(ships{ii}).range(interpbin);
+           Ships.(ships{ii}).azimuth = Ships.(ships{ii}).azimuth(interpbin);
+           Ships.(ships{ii}).dtime = Ships.(ships{ii}).dtime(interpbin);      
        end
-   end
-   clf;
-   clc;
-end
-%}
-%%
-% % % load the tracks from Velella acoustic data VLA2 % % %
-ii = 4;
-dnum_start = Ships.(ships{index(ii)}).dtime(xlims(ii,1));
-dnum_end = Ships.(ships{index(ii)}).dtime(xlims(ii,2));
-jd = floor(dnum_start-datenum(2017,1,1)+1);
 
-% create a time string for extracting data file names
-if floor(dnum_end-datenum(2017,1,1)+1) ~= jd
-    if minute(dnum_start)+1 == 60
-        ds = datestr(datenum(datenum(2017,1,jd,hour(dnum_start)+1,0,0):minutes(1):datenum(2017,1,floor(dnum_end-datenum(2017,1,1)+1),hour(dnum_end),minute(dnum_end),0)));
-    else
-        ds = datestr(datenum(datenum(2017,1,jd,hour(dnum_start),minute(dnum_start)+1,0):minutes(1):datenum(2017,1,floor(dnum_end-datenum(2017,1,1)+1),hour(dnum_end),minute(dnum_end),0))); 
+       % % plotting for GUI ship track selection % %
+       [ind, xl, xlr] = select_ships(Ships.(ships{ii}), ships{ii}, rawbin, ii);
+       clf; clc;
+       if isempty(ind); break; end % end the loop if user is done 
+       index = [index; ind];
+       xlims = [xlims; xl]; % interpolated endpoints
+       raw_xlims = [raw_xlims; xlr]; % raw data endpoints
+
     end
+    close all;
+    clc;
+    save('preloaded','index','xlims')
+    
 else
-    if minute(dnum_start)+1 == 60
-        ds = datestr(datenum(datenum(2017,1,jd,hour(dnum_start)+1,0,0):minutes(1):datenum(2017,1,jd,hour(dnum_end),minute(dnum_end),0)));
-    else
-        ds = datestr(datenum(datenum(2017,1,jd,hour(dnum_start),minute(dnum_start)+1,0):minutes(1):datenum(2017,1,jd,hour(dnum_end),minute(dnum_end),0))); 
-    end  
+    load('preloaded');      
 end
 
-% load and process the data
+% % % data loading & processing % % %
+
+% % define parameters
 fs = 25000; % sampling rate
 N = 16; % # of hydrophones
 nfft = fs;
 snapshot_avg = 1; % # snapshots to average, in seconds
 F1 = 50; % bottom frequency
 F2 = 200; % top frequency 
-df = floor(3 / (fs/nfft)); % desired freq. spacing, in bins
-
+df = floor(1 / (fs/nfft)); % desired freq. spacing, in bins
 f = [0:fs/nfft:(fs/2-fs/nfft),-fs/2:fs/nfft:-fs/nfft];
 f_ind = find(f>=F1 & f<=F2);
 f_ind = f_ind(1:df:end);
 
-S = size(ds,1); % number of minutes
-L = (60*fs/nfft-snapshot_avg+1);
-%D = zeros(S*60*fs/nfft,N,length(f_ind)); % initialize data matrix
-output = zeros(S*L, N*(N+1)*length(f_ind)); % initialize feature matrix
+for ii = 1:length(index) % loop over selected ships
+    clear output out
+    disp(['Ship: ' ships{index(ii)}]);
 
-tic;
-for dd = 1:S
-    tic;
-    hrs = ds(dd,13:14);
-    mins = ds(dd,16:17);
-
-    name = ['RAVA02.170' num2str(jd) hrs mins '00.000.sio'];
-    %ssh2_struct = ssh2_config('velella.ucsd.edu',username,password,22);
-    %x = scp_get(ssh2_struct,name,'','../../project/SCE17/acoustic/SCE17_VLA2_sio/');
-    x = sioread([pathtodata name],1,0,0);
-    delete('RAVA02*');
-    % process data
-    d = process_data(x,nfft,N,f_ind);
-   % D([1:(60*fs/nfft)]+(dd-1)*60*fs/nfft,:,:) = d; % dimensions: samples x sensors x frequencies
+    % create a time string for extracting data file names
+    dnum_start = Ships.(ships{index(ii)}).dtime(xlims(ii,1));
+    dnum_end = Ships.(ships{index(ii)}).dtime(xlims(ii,2));
+    jd = floor(dnum_start-datenum(2017,1,1)+1);
+    if floor(dnum_end-datenum(2017,1,1)+1) ~= jd
+        ds = datestr(datenum(datenum(2017,1,jd,hour(dnum_start),minute(dnum_start),0):minutes(1):datenum(2017,1,floor(dnum_end-datenum(2017,1,1)+1),hour(dnum_end),minute(dnum_end),0))); 
+    else
+        ds = datestr(datenum(datenum(2017,1,jd,hour(dnum_start),minute(dnum_start),0):minutes(1):datenum(2017,1,jd,hour(dnum_end),minute(dnum_end),0))); 
+    end
     
-    % create input feature matrix
-    output([1:L] + (dd-1)*L,:) = generate_features(d,snapshot_avg,nfft,fs);
-    %output{dd} = generate_features(d,snapshot_avg,nfft,fs);
-    toc
-end
-toc
+    % update and delimit label vectors
+    range = [floor(Ships.(ships{index(ii)}).range(xlims(ii,1):xlims(ii,2))*100)/100;...
+    Ships.(ships{index(ii)}).dtime(xlims(ii,1):xlims(ii,2))]; % delimit range and time vectors
 
-% save
-save([savepath 'input_' ships{ii} '_VLA2'],'-ascii',output);
+    % load and process data
+    tic;
+    disp('Processing....')
+    S = size(ds,1); % number of minutes
+    L = (60*fs/nfft-snapshot_avg+1); % number of segments, after averaging
+    parfor dd = 1:S
+        hrs = ds(dd,13:14);
+        mins = ds(dd,16:17);
+        name = ['RAVA02.170' num2str(jd) hrs mins '00.000.sio'];
+        
+        % check if file exists in acoustic folder
+        if exist([pathtodata name], 'file')==2
+           x = sioread([pathtodata name],1,0,0);
+        else
+            disp('Sorry, this file does not exist.');
+            pause(2);
+            continue;
+        end
+
+        % account for partial records
+        if dd==1
+            xstr = datestr(dnum_start);
+            secs = str2double(xstr(19:20));
+            if secs~=0
+                x = x(floor(secs*fs):end,:);
+            end
+        elseif dd==S
+            xstr = datestr(dnum_end);
+            secs = str2double(xstr(19:20));
+            if secs~=0
+                x = x(1:(end-floor((60-secs-1)*fs)),:);
+            end
+        end
+        
+        % process data
+        d = process_data(x,nfft,N,f_ind);
+        %D{dd} = d; % dimensions: samples x frequencies x sensors
+    
+        % create input feature matrix
+        output{dd} = generate_features(d,snapshot_avg,nfft,fs);
+        %output{dd} = abs(d(:,:));
+    end
+    disp(['     ' num2str(toc,'%.1f') ' s.']);
+    
+    % save, if any files found
+    out = [];
+    disp('Saving....');
+    if exist('output')
+        for dd = 1:S
+            out = [out; output{dd}];
+        end        
+        
+        % name according to N/S orientation (re VLA2)
+        if ~sum(Ships.(ships{index(ii)}).lat(raw_xlims(ii,1):raw_xlims(ii,2))>VLA2(2))
+            orien = 'S';
+        elseif ~sum(Ships.(ships{index(ii)}).lat(raw_xlims(ii,1):raw_xlims(ii,2))<VLA2(2))
+            orien = 'N';
+        else
+            disp('Warning: ship may cross VLA 2 latitutde. Saving as S.');
+            orien = 'S';
+        end
+        
+        save([savepath 'input_' orien ships{index(ii)} '_VLA2'],'out');
+        save([savepath 'range_' orien ships{index(ii)} '_VLA2'],'range');
+        
+    else 
+       pause(1);
+       clc;
+       continue;
+    end     
+    disp(['     ' num2str(toc,'%.1f') ' s.']);
+    pause(1);
+    clc;
+end
+
+% % % functions % % %
+
+function [index, x_lims, raw_xlims] = select_ships(ship, shipname, raw_index, ii)
+   plot(ship.dtime, ship.range);
+   hold on;
+   scatter(ship.dnum(raw_index), ship.(raw_index),'filled');
+   ylabel('Range (km)');
+   xlabel('Date, Time');
+   datetick('keeplimits');
+   title(['Ship name: ' shipname]);
+   legend('Interpolated range','Raw range');
+   s = input('Do you want to use this ship track?  (Y/N)','s');
+   
+   if strcmp(s,'Y')
+      index = ii; 
+      s = input('Do you want to use the whole time? (Y/N)','s');
+      if strcmp(s,'N')
+          disp('Select two points on the plot: starting point and ending point.');
+          dg = ginput(2);
+          xlim([dg(1,1) dg(2,1)]);
+          datetick('keeplimits');
+          s = input('Does that look better? (Y/N)','s');
+          if strcmp(s,'N')
+              disp('Ok, select two more endpoints please...');
+              dg = ginput(2);
+              disp('Displaying your final selection.');
+              xlim([dg(1,1) dg(2,1)]);
+              datetick('keeplimits');
+              pause(2);
+              disp('Moving on to next ship.');
+          end
+          [~,I1] = min(abs(dg(1,1) - ship.dtime));
+          [~,I2] = min(abs(dg(2,1) - ship.dtime));
+          x_lims = [I1, I2];
+          
+          [~,Ia] = min(abs(dg(1,1) - ship.dnum));
+          [~,Ib] = min(abs(dg(2,1) - ship.dnum));
+          raw_xlims = [Ia, Ib];
+      else
+         x_lims = [1, length(ship.dtime)]; 
+      end
+   elseif strcmp(s,'N')
+       s = input('Are you done? (Y/N)','s');
+       if strcmp(s,'Y')
+           index = [];
+           x_lims = 0;
+           disp('Thanks, closing program...');
+           pause(1);
+       end
+   end
+end
 
 function d = process_data(x,nfft,N,f_ind)
     M = floor(size(x,1)/nfft);
     win = hanning(nfft);
-    Y = reshape(x,[M,N,nfft]);
-    win = reshape(win.',[1,1,nfft]);
-    win = repmat(win,[M,N,1]);
-    X = fft(Y.*win,[],3);
-    d = X(:,:,f_ind);
+    Y = reshape(x(1:M*nfft,:),[nfft,M,N]);
+    win = repmat(win,[1,M,N]);
+    X = fft(Y.*win,[],1);
+    d = 2*X(f_ind,:,:)./nfft;
 end
 
 function features = generate_features(d,snap_avg,nfft,fs)
     snap_avg = floor(snap_avg*nfft/fs); % convert to bins
-    csdm = zeros(size(d,1),size(d,2),size(d,2),size(d,3));
-    csdm_avg = zeros(size(d,1)-snap_avg, size(d,2),size(d,2),size(d,3));
-    for tt = 1:size(d,1)
-        for ff = 1:size(d,3)
-            dat = squeeze(d(tt,:,ff)).';
+    csdm = zeros(size(d,2),size(d,3),size(d,3),size(d,1));
+    csdm_avg = zeros(size(d,2)-snap_avg+1, size(d,3),size(d,3),size(d,1));
+    for tt = 1:size(d,2)
+        for ff = 1:size(d,1)
+            dat = squeeze(d(ff,tt,:));
             dat = dat./norm(dat);
             csdm(tt,:,:,ff) = dat*dat';
         end
     end
     
     % snapshot averaging and feature creation
-    features = zeros(size(d,1)-snap_avg+1,(size(d,2)+1)*size(d,2)*size(d,3));
-    for tt = 1:(size(d,1)-snap_avg+1)
+    features = zeros(size(d,2)-snap_avg+1,(size(d,3)+1)*size(d,3)*size(d,1));
+    for tt = 1:(size(d,2)-snap_avg+1)
         csdm_avg(tt,:,:,:) = mean(csdm(tt:(tt+snap_avg-1),:,:,:),1); 
         feat = [];
-        for ff = 1:size(d,3)
-            for rr = 1:size(d,2)
+        for ff = 1:size(d,1)
+            for rr = 1:size(d,3)
                 feat = [feat,squeeze(csdm_avg(tt,rr,rr:end,ff)).'];
             end
         end
