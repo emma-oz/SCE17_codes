017142fdebecb8f67e606fe91f3a208f99a17747
ecreeves <ecreeves@ucsd.edu>
2018-04-03 20:38:04 -0700

    updating

--- make_csdm.m
+++ make_csdm.m
@@ -1,23 +1,25 @@
 clear; clc;
 
-fselect = 1:3:150; % frequency indices, for freq = 75:1:350 Hz vector
-path = '../../../Desktop/SCE17_VLA_range/data1/'; % path to data
+fselect = 1:151; % frequency indices, for freq = 75:1:350 Hz vector
+path = '../../../Desktop/SCE17_source_tow/';%'../../../Desktop/SCE17_VLA_range/data1/'; % path to data
 
-files = dir([path,'VLA2*.mat']);
+files = dir([path,'SCE17_simulation.mat']);
 
 D = struct();
 for fi=1:length(files)
     tic;
     fname = files(fi).name(1:end-4);
     data = load([path, fname]);
-    D.(fname) = data.D;
-    C = nan([size(D.(fname),2),size(D.(fname),2)]);
-    features = nan([size(D.(fname),1),(size(D.(fname),2)+1)*(size(D.(fname),2))/2*2*length(fselect)]);
-    for k = 1:size(D.(fname),1)
+   % fname(18) = 'n';
+   data.p = data.pm{1};
+    D.(fname) = data.p;%m{1};
+    C = nan([size(D.(fname),1),size(D.(fname),1)]);
+    features = nan([size(D.(fname),2),(size(D.(fname),1)+1)*(size(D.(fname),1))/2*2*length(fselect)]);
+    for k = 1:size(D.(fname),2)
         ftick=0;
         tmp = [];
         for f = fselect
-            C = D.(fname)(k,:,f).'*conj(D.(fname)(k,:,f));
+            C = D.(fname)(:,k,f)*D.(fname)(:,k,f)';
             for r = 1:size(C,1)
                 tmp = [tmp, C(r,r:end)];
             end
@@ -25,7 +27,7 @@ for fi=1:length(files)
         features(k,:) = [real(tmp),imag(tmp)];
     end
     
-    save(['input_' fname '.txt'],'-ascii','features');
+    save(['input_' fname],'features');
     toc
 end
 
